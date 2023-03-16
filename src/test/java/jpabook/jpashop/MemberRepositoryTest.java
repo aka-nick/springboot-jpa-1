d10dc15537155d2db0c3788aa48dad94a726d1e3
aka-nick <bechalrang@gmail.com>
2023-03-16 17:15:03 +0900

    섹션1 -  섹션1 프로젝트 설정 완료

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -3,13 +3,10 @@ package jpabook.jpashop;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MemberRepositoryTest {
 
@@ -27,5 +24,6 @@ class MemberRepositoryTest {
         Member find = memberRepository.find(id);
 
         assertThat(find.getUsername()).isEqualTo("memberA");
+        assertThat(find).isEqualTo(member); // 같은 영속성 컨텍스트 안에서는 동등성 보장
     }
 }
