2db81d566dfce2331a63d7c7854fbf1e17dbda03
aka-nick <bechalrang@gmail.com>
2023-03-18 10:43:52 +0900

    섹션2 -  엔티티 클래스 개발 완료(값타입(address) 생성자 추가)

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -4,11 +4,19 @@ import javax.persistence.Embeddable;
 import lombok.Getter;
 
 @Embeddable
-@Getter
+@Getter //값타입은 세터를 만들지 말자
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
