91704f98d6bbc7b0cbc4c6eb2a09dafb6d64aced
aka-nick <bechalrang@gmail.com>
2023-03-21 12:39:47 +0900

    섹션4 -  회원 도메인 개발 - 리포지토리 개발

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,29 +1,24 @@
 package jpabook.jpashop;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    public void testMember() {
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        Long id = memberRepository.save(member);
-
-        Member find = memberRepository.find(id);
-
-        assertThat(find.getUsername()).isEqualTo("memberA");
-        assertThat(find).isEqualTo(member); // 같은 영속성 컨텍스트 안에서는 동등성 보장
-    }
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    public void testMember() {
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        Long id = memberRepository.save(member);
+//
+//        Member find = memberRepository.find(id);
+//
+//        assertThat(find.getUsername()).isEqualTo("memberA");
+//        assertThat(find).isEqualTo(member); // 같은 영속성 컨텍스트 안에서는 동등성 보장
+//    }
 }
