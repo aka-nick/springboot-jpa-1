ef7d1985531a62d4747ca6ab7e461ab5f5d50bf8
aka-nick <bechalrang@gmail.com>
2023-03-22 11:21:59 +0900

    섹션5 - 웹 계층 개발 - 상품 화면 추가

--- src/main/java/jpabook/jpashop/contoller/ItemController.java
+++ src/main/java/jpabook/jpashop/contoller/ItemController.java
@@ -5,7 +5,6 @@ import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
