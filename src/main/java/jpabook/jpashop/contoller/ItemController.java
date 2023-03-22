3d07fe4fa78e4d94a387ddfb2d0b27c8b2949780
aka-nick <bechalrang@gmail.com>
2023-03-22 11:33:11 +0900

    섹션5 - 웹 계층 개발 - 상품 수정 컨트롤러 추가

--- src/main/java/jpabook/jpashop/contoller/ItemController.java
+++ src/main/java/jpabook/jpashop/contoller/ItemController.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop.contoller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,6 +39,23 @@ public class ItemController {
     public String list(Model model) {
         model.addAttribute("items", itemService.findItems());
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book find = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(find.getId());
+        form.setName(find.getName());
+        form.setPrice(find.getPrice());
+        form.setIsbn(find.getIsbn());
+        form.setAuthor(find.getAuthor());
+        form.setStockQuantity(find.getStockQuantity());
+
+        model.addAttribute("form", form);
+
+        return "items/updateItemForm";
     }
 
 }
