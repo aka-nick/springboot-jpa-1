0fa249c445559cdbbf579b0d4ae281a27c9d05c6
aka-nick <bechalrang@gmail.com>
2023-03-22 17:29:00 +0900

    섹션3 - api 개발 고급 - 주문(xToOne 연관 관계) 조회 구현 - 2. DTO를 생성해서 노출하는 경우

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,10 +1,14 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,9 +33,29 @@ public class OrderSimpleApiController {
                     o.getDelivery().getAddress();   // 강제 조회
                     return o;
                 }).collect(Collectors.toList());
-
-
     }
 
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrdersDto> ordersV2() {
+        return orderRepository.findAll(new OrderSearch()).stream()
+                .map(SimpleOrdersDto::new)
+                .collect(Collectors.toList());
+    }
+    @Data
+    static class SimpleOrdersDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrdersDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
 
 }
