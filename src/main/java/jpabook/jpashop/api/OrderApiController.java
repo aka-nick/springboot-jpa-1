aafc0006b041cfdca6cc3659edbb4442e5e326a3
aka-nick <bechalrang@gmail.com>
2023-03-23 16:01:11 +0900

    섹션4 - api 개발 고급 - OneToMany 연관 관계 - 조회 v1 구현

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.api;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        return orderRepository.findAll(new OrderSearch())
+                .stream()
+                .map(o -> {
+                    o.getMember().getName();
+                    o.getDelivery().getAddress();
+                    o.getOrderItems().stream().forEach(oi -> oi.getItem().getName());
+                    return o;
+                })
+                .collect(Collectors.toList());
+    }
+
+
+
+}
